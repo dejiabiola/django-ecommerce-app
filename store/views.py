a48c7b790a068b0f95e512287fc9f845a6886ec5
Adedeji Abiola <adedejiabiola56@gmail.com>
2021-04-10 23:51:43 +0100

    setup templates for index, cart and checkout pages

--- store/views.py
+++ store/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 
 
 def index(request):
-  return render(request, 'store/index.html')
+  context = {}
+  return render(request, 'store/index.html', context)
+
+
+def cart(request):
+  context = {}
+  return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+  context = {}
+  return render(request, 'store/checkout.html', context)
