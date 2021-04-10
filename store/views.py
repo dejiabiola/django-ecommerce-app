7e69ece1abe4e1ebb3222bb2c7a5e03cfdedc9a8
Adedeji Abiola <adedejiabiola56@gmail.com>
2021-04-11 00:24:51 +0100

    rendered products in index page

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-  context = {}
+  products = Product.objects.all()
+  context = {'products': products}
   return render(request, 'store/index.html', context)
 
 
