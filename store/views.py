2246253dd284b32f35735453519e48acf051033c
Adedeji Abiola <adedejiabiola56@gmail.com>
2021-04-17 03:51:57 +0100

    buy product and view orders in cart page functionality done for both signed in and guest users

--- store/views.py
+++ store/views.py
@@ -4,8 +4,35 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import CreateUserForm, ProductForm
+
+
+class Basket:
+  # a data transfer object to shift items from cart to page. Used for guest login only
+  
+  def __init__(self, id, name, price, image_url, quantity):
+    self.id = id
+    self.name = name
+    self.price = price
+    self.quantity = quantity
+    self.image_url = image_url
+
+  def get_total(self):
+    return self.quantity * self.price
+
+
 # Create your views here.
 
+
+
+# convenience method as used in several methods
+def get_basket(request):
+  basket = request.session.get('basket', [])
+  products = []
+  for item in basket:
+    product = Product.objects.get(id=item[0])
+    basket = Basket(item[0], product.name, product.price, product.image_url, item[1])
+    products.append(basket)
+  return products
 
 def user_login(request):
   # if user is already logged in, don't show him the login page
@@ -90,13 +117,31 @@ def product_detail(request, id):
   return render(request, 'store/product_detail.html', context)
 
 def product_buy(request):
-  if request.method== "POST":
-      temp_id = int(request.POST.get('id',''))
-      quantity = int(request.POST.get('quantity', ''))
-      basket = request.session['basket']
-      basket.append([temp_id,quantity])
-      request.session['basket'] = basket
-  return redirect('product_list')
+  try:
+    guest_login = request.session['user']
+  except KeyError:
+    guest_login = None
+    print("We have landed", guest_login)
+  if guest_login == 'guest' or request.user.is_authenticated: 
+    if request.method== "POST":
+        product_id = int(request.POST.get('id',''))
+        quantity = int(request.POST.get('quantity', ''))
+        if request.user.is_authenticated:
+          customer = request.user.customer
+          product = Product.objects.get(id=product_id)
+          order, created = Order.objects.get_or_create(customer=customer, complete=False) 
+          line_item, created = LineItem.objects.get_or_create(order=order, product=product)
+          line_item.quantity = quantity
+          line_item.save()
+        else:
+          basket = request.session.get('basket', [])
+          basket.append([product_id,quantity])
+          request.session['basket'] = basket
+
+    return redirect('index')
+  else:
+    messages.info(request, "Please login or continue as guest to purchase a product")
+    return redirect('login')
 
 def product_new(request):
     if request.method=="POST":
@@ -123,6 +168,7 @@ def product_edit(request, id):
         form = ProductForm(instance=product)
     return render(request, 'shop/product_edit.html', {'form': form})
 
+# Delete Product By Admin Logic
 def product_delete(request, id):
     product = get_object_or_404(Product, id=id)
     deleted = request.session.get('deleted', 'empty')
@@ -131,18 +177,30 @@ def product_delete(request, id):
     messages.success(request, product.name + ' has been deleted successfully')
     return redirect('index')
 
+
+# Cart page logic
 def cart(request):
+  guest_login = request.session.get('user', '')
   user = request.user
+  # use order and line item models if user is signed in with username
   if user.is_authenticated:
     customer = user.customer
     order, created = Order.objects.get_or_create(customer=customer, complete=False) 
     items = order.lineitem_set.all()
+  # if user is signed in as guest, use sessions to hold the basket
+  elif guest_login == 'guest':
+    items = get_basket(request)
+    total_amount = 0
+    total_quantity = 0
+    for item in items:
+      total_amount += item.get_total()
+      total_quantity += item.quantity
+    order = {'get_cart_total': total_amount, 'get_cart_items': total_quantity}
   else:
     items = []
     order = {'get_cart_total': 0, 'get_cart_items': 0}
-  context = {'items': items, 'order': order}
+  context = {'items': items, 'order': order, 'guest_login': guest_login}
   return render(request, 'store/cart.html', context)
-
 
 def checkout(request):
   user = request.user
